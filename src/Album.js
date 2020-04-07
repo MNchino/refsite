0ae1c51644240511db9797417314f9a13210e153
Supreme Overlord of the Universe <16561590+MNchino@users.noreply.github.com>
2020-04-07 02:44:36 -0400

    initialized

---
+++ src/Album.js
@@ -0,0 +1,165 @@
+import React, {useState, useEffect} from 'react'; 
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import siglogo from './signature.png';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import Slide from '@material-ui/core/Slide';
+
+const getMuiTheme = ()=> createMuiTheme({
+  palette: {
+    primary: {
+      main:  "#43a047",
+    }
+  },
+});
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'...And thank all you artists who made this possible!'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '100%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export default function Album() {
+  const classes = useStyles();
+  const [listOfImages, setListOfImages] = useState([]);
+  function importAll(r) {
+    return r.keys().map(r);
+  }
+  function cutName(name)
+  {
+    var temp = name.match("[^\/]+$");
+    temp = ("").concat(temp);
+    temp = temp.substr(0, temp.indexOf("#"))
+    return temp;
+  }
+  function cutArtist(name)
+  {
+    var temp = name.match("[^\/]+$");
+    temp = ("").concat(temp);
+    temp = temp.substr(temp.indexOf("#") + 1)
+    temp = temp.substr(0, temp.indexOf("."))
+    return temp;
+  }
+
+  useEffect(() => 
+  {
+    setListOfImages(importAll(require.context('./images/', false, /\.(png|jpe?g|svg)$/)));
+  }, [])
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <MuiThemeProvider theme={getMuiTheme()}>
+      <AppBar src="relative">
+        <Toolbar>
+          <img src={siglogo} className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Reference Sheet Site
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              <br/>You found my refsite!
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Hiya! I slapped this quick site together in React so ya'll could access my refs without me having to post 'em all the time. Feel free to have a look!
+            </Typography>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {listOfImages.map((image, index) => (
+              <Grid item key={index} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={image}
+                    title={image.match("[^\/]+$")}
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                    {cutName(image)}
+                    </Typography>
+                    <Typography>
+                      Artist: {cutArtist(image)}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button href={image} size="small" color="primary">
+                      View
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Thank you for visiting!
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          You're really awesome for checking this all out.
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+      </MuiThemeProvider>
+    </React.Fragment>
+  );
+}
